42a4be1b014d4f3498581f7b3570b6642c174eed
rarbad <arbadrevati@gmail.com>
2025-02-12 23:06:28 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# extract username from email
+# email=input("enter your email")
+# username=input("enter your username")
+# print(email)
+# print(username)
+
+#  replace every 'a'in a string with @
+# a= "revatiarbad"
+# print(a.replace("a","@"))
+ 
+# reverse a string without using reverse()
+# input_string= input ("enter your name")
+# surname=input_string[::-1]
+# print("reversed string:",surname)
+
+# count the number of vowels in a the string
+# name=input("enter your name")
+# vowel_count= sum(name.count(vowel)for vowel in "aeiou")
+# print("number of vowel in the name:",{vowel_count})
